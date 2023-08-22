a53b17b578accb9e3324a13c42fb5b01cc97055b
MDNET-Developer <qarabagli.murad@mail.ru>
2023-08-22 09:30:18 +0400

    Update Popular Locations

---
+++ DapperRealEstateWebUI/ViewComponents/HomePage/_DefaultLocationListViewComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DapperRealEstateWebUI.ViewComponents.HomePage
+{
+    public class _DefaultLocationListViewComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
