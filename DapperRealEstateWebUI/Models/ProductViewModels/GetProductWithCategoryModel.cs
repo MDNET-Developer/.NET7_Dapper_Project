5529b20023fc0f9d6356ca248f5b6e7a43bf7d5e
MDNET-Developer <qarabagli.murad@mail.ru>
2023-09-22 17:18:31 +0400

    Consume data to UI

---
+++ DapperRealEstateWebUI/Models/ProductViewModels/GetProductWithCategoryModel.cs
@@ -0,0 +1,15 @@
+﻿namespace DapperRealEstateWebUI.Models
+{
+    public class GetProductWithCategoryModel
+    {
+        public int ProductId { get; set; }
+        public string? Title { get; set; }
+        public decimal Price { get; set; }
+        public string? CoverImage { get; set; }
+        public string? City { get; set; }
+        public string? District { get; set; }
+        public string? Address { get; set; }
+        public string? Description { get; set; }
+        public string? CategoryName { get; set; }
+    }
+}
--- DapperRealEstateWebUI/ViewComponents/HomePage/_DefaultHomeProductListViewComponentPartial.cs
+++ DapperRealEstateWebUI/ViewComponents/HomePage/_DefaultHomeProductListViewComponentPartial.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DapperRealEstateWebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace DapperRealEstateWebUI.ViewComponents.HomePage
 {
     public class _DefaultHomeProductListViewComponentPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DefaultHomeProductListViewComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var responseMessage = await _httpClientFactory.CreateClient().GetAsync("https://localhost:7008/api/Product/GetProductWithCategory");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<IEnumerable<GetProductWithCategoryModel>>(jsonData);
+                return View(values);
+            }
+
             return View();  
         }
     }
